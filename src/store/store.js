de80633c47a7dad1711d14a22ba43c8a72f36eee
George Kimagut <geojimagut@gmail.com>
2025-06-18 12:04:43 +0300

    <theme colors for randomization of hero section>

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+//theme colors
+export const theme_colors = [
+  { color_name: "default_color", the_color: "#131f6b" },
+  { color_name: "white_color", the_color: "#f5f5f5" },
+  { color_name: "secondary_color", the_color: "#82bc00" },
+  { color_name: "third_color", the_color: "#007cba" },
+];
